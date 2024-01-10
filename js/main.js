8a08ebeb37b595ad1c6ccdecf7596862443867ff
MEHDI-HAMDI-Yousra-2326076aa <yousra.MEHDI-HAMDI@etu.univ-amu.fr>
2024-01-10 08:22:22 +0100

    continue

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+document.getElementById('envoyer').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById('email');
+    const subject = document.getElementById('subject');
+    const mess = document.getElementById('Contact');
+
+    const bodymessage = `Email : ${email.value}<br> Subject: ${subject.value}<br> Message: ${mess.value}`;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "chessweb7@gmail.com",
+        Password : "8E95DF2BFE4225A64EC31EBB47EC0777DCB9",
+        Port : 2525,
+        To: 'chessweb7@gmail.com',
+        From: document.getElementById("email").value,
+        Subject: subject.value,
+        Body: bodymessage
+    }).then(
+        message => {
+            alert(message);
+            myalert();
+        }
+    );
+});
+
 document.getElementById('theme').addEventListener('click', function(){
     css = document.getElementById('css');
     if(css.href.endsWith("/css/theme.css")) {
@@ -6,7 +32,11 @@ document.getElementById('theme').addEventListener('click', function(){
         css.href='css/theme.css';
     }
 });
-document.getElementById('envoyer').addEventListener('onclick',function (){
-    alert(' Message envoyé !');
-});
+function myalert() {
+    alert("Merci pour votre retour !");
+}
+
+
+
+
 
