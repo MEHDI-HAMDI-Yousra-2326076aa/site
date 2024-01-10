3d9ad24c6f521516b75e2a4ecaf1699d7821fdf0
Sebbak <mehdi.sebbak@etu.univ-amu.fr>
2024-01-10 22:04:11 +0100

    On fait un autre update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-if(document.getElementById('envoyer')) document.getElementById('envoyer').addEventListener('click', function (e) {
+if (document.getElementById('envoyer')) document.getElementById('envoyer').addEventListener('click', function (e) {
     e.preventDefault();
 
     const email = document.getElementById('email');
@@ -8,10 +8,10 @@ if(document.getElementById('envoyer')) document.getElementById('envoyer').addEve
     const bodymessage = `Email : ${email.value}<br> Subject: ${subject.value}<br> Message: ${mess.value}`;
 
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "chessweb7@gmail.com",
-        Password : "8E95DF2BFE4225A64EC31EBB47EC0777DCB9",
-        Port : 2525,
+        Host: "smtp.elasticemail.com",
+        Username: "chessweb7@gmail.com",
+        Password: "8E95DF2BFE4225A64EC31EBB47EC0777DCB9",
+        Port: 2525,
         To: 'chessweb7@gmail.com',
         From: document.getElementById("email").value,
         Subject: subject.value,
@@ -24,29 +24,32 @@ if(document.getElementById('envoyer')) document.getElementById('envoyer').addEve
     );
 });
 
-if(document.getElementById('theme')) document.getElementById('theme').addEventListener('click', function(){
+if (document.getElementById('theme')) document.getElementById('theme').addEventListener('click', function () {
     css = document.getElementById('css');
-    if(css.href.endsWith("/css/theme.css")) {
-        css.href='css/style.css'
+    if (css.href.endsWith("/css/theme.css")) {
+        css.href = 'css/style.css'
     } else {
-        css.href='css/theme.css';
+        css.href = 'css/theme.css';
     }
 });
+
 function myalert() {
     alert("Merci pour votre retour !");
 }
+
 function ouvrir() {
     let videoContainer = document.getElementById('video');
 
     videoContainer.style.display = 'block';
 }
+
 function fermer() {
     let videoContainer = document.getElementById('video');
 
     videoContainer.style.display = 'none';
 }
-if(document.getElementById('btnfermer')) document.getElementById('btnfermer').addEventListener('click',fermer);
 
+if (document.getElementById('btnfermer')) document.getElementById('btnfermer').addEventListener('click', fermer);
 
 
 const menuHamburger = document.querySelector(".menuHamburger");
@@ -58,3 +61,20 @@ if (menuHamburger) {
 
     });
 }
+document.getElementById('changementTheme').addEventListener('click', function () {
+        var classique = document.querySelectorAll('.classique');
+        var traditionnel = document.querySelectorAll('.traditionnel');
+        classique.forEach((element,index) => {
+
+            if (element.disabled) {
+                element.disabled = false;
+                traditionnel[index].disabled = true;
+            } else {
+                element.disabled = true;
+                traditionnel[index].disabled = false;
+            }
+        });
+
+    }
+);
+
